bd1b1d8017e54805b891d8b438e7d162d58e9974
viktorsakman <viktorsakman@gmail.com>
2023-11-04 04:05:26 -0500

    Added Map Functionalities

--- app/src/main/java/com/cs407/lab6_milestone1/MainActivity.java
+++ app/src/main/java/com/cs407/lab6_milestone1/MainActivity.java
@@ -1,14 +1,24 @@
 package com.cs407.lab6_milestone1;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity {
-
+    // Bascom Hall Positioning
+    private final LatLng mDestinationLatLng = new LatLng(43.07575740887715, -89.40400103580576);
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_map);
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+            // code to display marker
+            googleMap.addMarker(new MarkerOptions().position(mDestinationLatLng).title("Destination"));
+        });
     }
 }
